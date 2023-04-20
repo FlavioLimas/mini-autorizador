0e05a15dddcea0edc109f1b36e97e42ea0daef4f
Flavio Lima <flaviolima.s@live.com>
2023-04-20 19:47:59 -0300

    controller teste

--- src/main/java/com/atorizepoc/miniautorizador/controller/DockerProductController.java
+++ src/main/java/com/atorizepoc/miniautorizador/controller/DockerProductController.java
@@ -1,6 +1,7 @@
 package com.atorizepoc.miniautorizador.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,8 @@ public class DockerProductController {
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/products")
-    public String getMessage() {
-        return "A brand new product";
+    public ResponseEntity<Object> getMessage() {
+        return ResponseEntity.ok("Hello from Docker!");
     }
 
 }
