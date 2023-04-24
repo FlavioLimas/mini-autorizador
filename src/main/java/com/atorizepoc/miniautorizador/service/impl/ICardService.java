4f86a50e44ee22aa4ba7430e2330618a8ecf5426
Flavio Lima <flaviolima.s@live.com>
2023-04-24 15:15:35 -0300

    WIP - definido interface para card

---
+++ src/main/java/com/atorizepoc/miniautorizador/service/impl/ICardService.java
@@ -0,0 +1,11 @@
+package com.atorizepoc.miniautorizador.service.impl;
+
+import com.atorizepoc.miniautorizador.external.dto.CardDTO;
+
+import java.util.List;
+
+public interface ICardService {
+
+    List<CardDTO> getAll();
+
+}
