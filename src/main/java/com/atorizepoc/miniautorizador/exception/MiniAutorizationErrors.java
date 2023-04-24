617e9c348d64c5254ae04ccd2aea19ae0c8d597e
Flavio Lima <flaviolima.s@live.com>
2023-04-24 18:46:01 -0300

    WIP - retirando nao necessario

--- src/main/java/com/atorizepoc/miniautorizador/exception/MiniAutorizationErrors.java
+++ src/main/java/com/atorizepoc/miniautorizador/exception/MiniAutorizationErrors.java
@@ -6,14 +6,9 @@ public enum MiniAutorizationErrors {
 
     CARDS_NOT_FOUND("Cards Not Found"),
     CARD_NOT_FOUND("Card Not Found"),
-    ASSOCIATE_NOT_FOUND("Associate Not Found"),
-    ASSOCIATE_NOT_SAVED("Associate Not Saved"),
-    ASSOCIATE_NOT_UPDATED("Associate Not Updated"),
     VALUE_INVALID("Value Invalid"),
     CARD_NOT_SAVED("Card Not Saved"),
-    CARD_NOT_UPDATED("Card Not Updated"),
-    ID_MUST_NOT_BE_NULL_WHEN_UPDATE("Id Must Not Be Null When Update"),
-    VOTE_INVALID("Vote Invalid");
+    CARD_NOT_UPDATED("Card Not Updated");
 
     @Getter
     private String message;
