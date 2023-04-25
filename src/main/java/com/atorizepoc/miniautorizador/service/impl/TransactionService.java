76fee49ef94bb86127a088888a0ae7c90c6040b7
Flavio Lima <flaviolima.s@live.com>
2023-04-25 16:56:10 -0300

    WIP - ajustes

--- src/main/java/com/atorizepoc/miniautorizador/service/impl/TransactionService.java
+++ src/main/java/com/atorizepoc/miniautorizador/service/impl/TransactionService.java
@@ -25,23 +25,25 @@ public class TransactionService implements ITransactionService {
     @Override
     @SneakyThrows
     public ResponseEntity<Object> executeTransaction(TransactionalDTO transactionalDTO) {
-        CardDTO existscardDTO = cardService.findByNumber(transactionalDTO.getNumeroCartao());
-        ResponseEntity<Object> response = checkTransaction(transactionalDTO, existscardDTO);
-        cardService.update(mapper.to(transactionalDTO, existscardDTO));
+        CardDTO existsCardDTO = cardService.findByNumber(transactionalDTO.getNumeroCartao());
+        ResponseEntity<Object> response = checkTransaction(transactionalDTO, existsCardDTO);
+        cardService.update(mapper.to(transactionalDTO, existsCardDTO));
         return response;
     }
 
     @SneakyThrows
-    private ResponseEntity<Object> checkTransaction(TransactionalDTO transactionalDTO, CardDTO existscardDTO) {
-
-        if (0 == existscardDTO.getValor().intValue()) {
+    private ResponseEntity<Object> checkTransaction(TransactionalDTO transactionalDTO, CardDTO existsCardDTO) {
+        if (0 == existsCardDTO.getValor().intValue()) {
+            log.info("Saldo existente " + existsCardDTO.getValor() + " Valor da transação " +
+                    transactionalDTO.getValor());
             return ResponseEntity.unprocessableEntity()
                     .body(MiniAutorizationErrors.INSUFFICIENT_FUNDS.getMessage());
-        } else if (!transactionalDTO.getSenhaCartao().equals(existscardDTO.getSenha())) {
+        } else if (!existsCardDTO.getSenha().equals(transactionalDTO.getSenhaCartao())) {
+            log.info("Senha existente " + existsCardDTO.getSenha() + "Senha da transação " +
+                    transactionalDTO.getSenhaCartao());
             return ResponseEntity.unprocessableEntity()
                     .body(MiniAutorizationErrors.INVALID_PASSWORD.getMessage());
         }
         return ResponseEntity.created(URI.create("/transacoes")).body(HttpStatus.OK);
     }
-
 }
